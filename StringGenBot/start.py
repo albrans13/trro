18895962d3992e4e1bdbba78d38eda565d966ec7
albrans13 <145933491+albrans13@users.noreply.github.com>
2023-10-19 15:39:26 +0300

    Add files via upload

--- StringGenBot/start.py
+++ StringGenBot/start.py
@@ -12,26 +12,23 @@ async def start(bot: Client, msg: Message):
     me2 = (await bot.get_me()).mention
     await bot.send_message(
         chat_id=msg.chat.id,
-        text=f"""**- مرحبـاً بــك {name} ,
+        text=f"""**- مرحبـاً بــك {name}
 
 - فـي بـوت استخـراج كـود تيرمكـس
 - يعمـل هـذا البـوت لمساعدتـك بطريقـة سهلـه 🏂
 - للحصـول على كـود تيرمكـس بـدون بـانـد🎗
 » قم باختيـار نـوع الجلسـه الان
-» لـ الحصـول على كـود ترمكس اضغـط الزر ( تلثيون)**
-""",
-
+» لـ الحصـول على كـود ترمكس اضغـط الزر ( تلثيون)**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(text="اضـغط لـبدا آسـتـخـراج الــكـود🫵ْٖ", callback_data="generate")
+                    InlineKeyboardButton(text="[{اضـعـط لـبـدا اسـتـخـراج الـكـود🛸}]", callback_data="generate")
                 ],
                 [
-                    InlineKeyboardButton("قـنـاة ســورس ️", url="https://t.me/def_Zoka"),
-  InlineKeyboardButton("مـطـوريـن الــبـوت ", url="https://t.me/def_Zoka/865"),
+                    InlineKeyboardButton(" مــطـوريـن الـبـوت ⚡️ ️", url="https://t.me/source_refz/865"),
+                    InlineKeyboardButton(" 🌐لـلـمسـاعـده", user_id=OWNER_ID)
                 ]
             ]
         ),
         disable_web_page_preview=True,
     )
-
